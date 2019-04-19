6c91f8010168f97ef807b13f9a3630676cb79a7e
Eugene Cheipesh <echeipesh@gmail.com>
2019-04-19 10:16:40 -0700

    Add SimpleIngest main

---
+++ build.sbt
@@ -0,0 +1,91 @@
+name := "geotrellis-workshop"
+organization := "geotrellis.batch"
+version := "0.1.0"
+
+scalaVersion := "2.11.12"
+
+libraryDependencies ++= Seq(
+  "com.monovore" %% "decline" % "0.6.2",
+  "org.locationtech.geotrellis" %% "geotrellis-spark" % "2.3.0",
+  "org.locationtech.geotrellis" %% "geotrellis-s3" % "2.3.0",
+  "com.azavea.geotrellis" %% "geotrellis-contrib-vlm" % "2.11.0",
+  "com.azavea.geotrellis" %% "geotrellis-contrib-gdal" % "2.11.0",
+  "org.apache.spark" %% "spark-core" % "2.4.0" % Provided,
+  "org.apache.spark" %% "spark-sql" % "2.4.0" % Provided,
+  "org.apache.spark" %% "spark-hive" % "2.4.0" % Provided
+)
+
+Test / fork := true
+Test / outputStrategy := Some(StdoutOutput)
+
+resolvers ++= Seq(
+  "LocationTech Snapshots" at "https://repo.locationtech.org/content/groups/snapshots",
+  "LocationTech Releases" at "https://repo.locationtech.org/content/groups/releases",
+  Resolver.url("typesafe", url("http://repo.typesafe.com/typesafe/ivy-releases/"))(Resolver.ivyStylePatterns),
+  Resolver.bintrayRepo("azavea", "geotrellis")
+)
+
+// Settings for sbt-assembly plugin which builds fat jars for spark-submit
+assemblyMergeStrategy in assembly := {
+  case "reference.conf"   => MergeStrategy.concat
+  case "application.conf" => MergeStrategy.concat
+  case PathList("META-INF", xs @ _*) =>
+    xs match {
+      case ("MANIFEST.MF" :: Nil) =>
+        MergeStrategy.discard
+      case ("services" :: _ :: Nil) =>
+        MergeStrategy.concat
+      case ("javax.media.jai.registryFile.jai" :: Nil) | ("registryFile.jai" :: Nil) | ("registryFile.jaiext" :: Nil) =>
+        MergeStrategy.concat
+      case (name :: Nil) if name.endsWith(".RSA") || name.endsWith(".DSA") || name.endsWith(".SF") =>
+        MergeStrategy.discard
+      case _ =>
+        MergeStrategy.first
+      }
+  case _ => MergeStrategy.first
+}
+
+// Settings from sbt-lighter plugin that will automate creating and submitting this job to EMR
+import sbtlighter._
+
+sparkEmrRelease := "emr-5.23.0"
+sparkAwsRegion := "us-east-1"
+sparkClusterName := "geotrellis-workshop"
+sparkEmrApplications := Seq("Spark", "Zeppelin", "Ganglia")
+sparkJobFlowInstancesConfig := sparkJobFlowInstancesConfig.value.withEc2KeyName("geotrellis-emr")
+sparkS3JarFolder := "s3://geotrellis-test/jobs/jars"
+sparkS3LogUri := Some("s3://geotrellis-test/jobs/logs")
+sparkMasterType := "m4.xlarge"
+sparkCoreType := "m4.xlarge"
+sparkInstanceCount := 5
+sparkMasterPrice := Some(0.5)
+sparkCorePrice := Some(0.5)
+sparkEmrBootstrap := List(
+  BootstrapAction("Install GDAL + dependencies",
+                  "s3://geotrellis-test/usbuildings/bootstrap.sh",
+                  "s3://geotrellis-test/usbuildings",
+                  "v1.0"))
+sparkEmrServiceRole := "EMR_DefaultRole"
+sparkInstanceRole := "EMR_EC2_DefaultRole"
+sparkEmrConfigs := List(
+  EmrConfig("spark").withProperties(
+    "maximizeResourceAllocation" -> "true"
+  ),
+  EmrConfig("spark-defaults").withProperties(
+    "spark.driver.maxResultSize" -> "3G",
+    "spark.dynamicAllocation.enabled" -> "true",
+    "spark.shuffle.service.enabled" -> "true",
+    "spark.rdd.compress" -> "true",
+    "spark.driver.extraJavaOptions" -> "-Djava.library.path=/usr/local/lib",
+    "spark.executor.extraJavaOptions" -> "-Djava.library.path=/usr/local/lib -XX:+UseParallelGC",
+    "spark.executorEnv.LD_LIBRARY_PATH" -> "/usr/local/lib"
+  ),
+  EmrConfig("spark-env").withProperties(
+    "LD_LIBRARY_PATH" -> "/usr/local/lib"
+  ),
+  EmrConfig("yarn-site").withProperties(
+    "yarn.resourcemanager.am.max-attempts" -> "1",
+    "yarn.nodemanager.vmem-check-enabled" -> "false",
+    "yarn.nodemanager.pmem-check-enabled" -> "false"
+  )
+)
---
+++ src/main/scala/SimpleIngest.scala
@@ -0,0 +1,104 @@
+package geotrellis.batch
+
+import geotrellis.raster._
+import geotrellis.raster.io.geotiff._
+import geotrellis.raster.render._
+import geotrellis.raster.resample._
+import geotrellis.raster.reproject._
+import geotrellis.proj4._
+
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.file._
+import geotrellis.spark.io.hadoop._
+import geotrellis.spark.io.index._
+import geotrellis.spark.pyramid._
+import geotrellis.spark.reproject._
+import geotrellis.spark.tiling._
+import geotrellis.spark.render._
+
+import geotrellis.vector._
+
+
+import cats.implicits._
+import com.monovore.decline._
+
+import org.apache.spark._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd._
+
+import scala.util.Properties
+
+object SimpleIngest extends CommandApp(
+  name = "geotrellis-workshop",
+  header = "Creates a Spark batch job",
+  main = {
+    val inputsOpt = Opts.option[String]("input", help = "The path that points to data that will be read")
+    val outputOpt = Opts.option[String]("output", help = "The path of the output catlaog")
+    val numPartitionsOpt = Opts.option[Int]("numPartitions", help = "The number of partitions to use").withDefault(100)
+
+    (inputsOpt, outputOpt, numPartitionsOpt).mapN { (inputs, output, numPartitions) =>
+      val conf = new SparkConf()
+        .setIfMissing("spark.master", "local[*]")
+        .setAppName("GeoTrellis SimpleIngest")
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        .set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
+        .set("spark.executionEnv.AWS_PROFILE", Properties.envOrElse("AWS_PROFILE", "default"))
+        .set("spark.driver.bindAddress", "127.0.0.1")
+
+      implicit val spark = SparkSession.builder.config(conf).enableHiveSupport.getOrCreate
+      implicit val sc = spark.sparkContext
+
+      // Read the geotiff in as a single image RDD,
+      // using a method implicitly added to SparkContext by
+      // an implicit class available via the
+      // "import geotrellis.spark.io.hadoop._ " statement.
+      val inputRdd: RDD[(ProjectedExtent, MultibandTile)] =
+        sc.hadoopMultibandGeoTiffRDD(inputs)
+
+      // Use the "TileLayerMetadata.fromRdd" call to find the zoom
+      // level that the closest match to the resolution of our source image,
+      // and derive information such as the full bounding box and data type.
+      val (_, rasterMetaData) =
+        TileLayerMetadata.fromRDD(inputRdd, FloatingLayoutScheme(256))
+
+      // Use the Tiler to cut our tiles into tiles that are index to a floating layout scheme.
+      // We'll repartition it so that there are more partitions to work with, since spark
+      // likes to work with more, smaller partitions (to a point) over few and large partitions.
+      val tiled: RDD[(SpatialKey, MultibandTile)] =
+        inputRdd
+          .tileToLayout(rasterMetaData.cellType, rasterMetaData.layout, Bilinear)
+          .repartition(numPartitions)
+
+      // We'll be tiling the images using a zoomed layout scheme
+      // in the web mercator format (which fits the slippy map tile specification).
+      // We'll be creating 256 x 256 tiles.
+      val layoutScheme = ZoomedLayoutScheme(WebMercator, tileSize = 256)
+
+      // We need to reproject the tiles to WebMercator
+      val (zoom, reprojected): (Int, RDD[(SpatialKey, MultibandTile)] with Metadata[TileLayerMetadata[SpatialKey]]) =
+        MultibandTileLayerRDD(tiled, rasterMetaData)
+          .reproject(WebMercator, layoutScheme, Bilinear)
+
+      // Create the attributes store that will tell us information about our catalog.
+      // Note: we're using SPI to select appropriate implementation
+      val attributeStore = AttributeStore(output)
+
+      // Create the writer that we will use to store the tiles in the local catalog.
+      // Note: AttributeStore and LayerWriter don't have to use the same thing
+      val writer = LayerWriter(attributeStore, output)
+
+      // Pyramiding up the zoom levels, write our tiles out to the local file system.
+      Pyramid.upLevels(reprojected, layoutScheme, zoom, Bilinear) { (rdd, z) =>
+        val layerId = LayerId("landsat", z)
+        // If the layer exists already, delete it out before writing
+        if(attributeStore.layerExists(layerId)) {
+          attributeStore.delete(layerId)
+        }
+        writer.write(layerId, rdd, ZCurveKeyIndexMethod)
+      }
+
+      spark.stop()
+    }
+  }
+)
